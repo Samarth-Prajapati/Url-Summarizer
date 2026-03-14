6c8938dcfb89e76854f4d95b7e87c6d7012ecba7
Samarth-Prajapati <samarthp475@gmail.com>
2026-03-14 20:10:44 +0530

    Add : Implemented summarizer for summarizing our urls.

---
+++ UrlSummarizer/services/summarizer.py
@@ -0,0 +1,46 @@
+from langchain_groq import ChatGroq
+from langchain_classic.chains.summarize import load_summarize_chain
+
+class Summarizer:
+    """Summarizer class that summarizes content using groq model"""
+
+    def __init__(self):
+        self.llm = None
+        self.chain = None
+
+    def load_llm(self, groq_api_key):
+        """
+        Loading Groq LLM model
+        :return: None
+        """
+
+        try:
+            self.llm = ChatGroq(
+                api_key = groq_api_key,
+                model = "llama-3.3-70b-versatile",
+                temperature = 0.3
+            )
+
+        except Exception as error:
+            print(error)
+
+    def create_chain(self, groq_api_key, prompt):
+        """
+        Creating Chain model
+        :return: chain
+        """
+
+        self.load_llm(groq_api_key)
+
+        try:
+            self.chain = load_summarize_chain(
+                self.llm,
+                chain_type = "stuff",
+                prompt = prompt
+            )
+            return self.chain
+
+        except Exception as error:
+            print(error)
+
+summarizer = Summarizer()
