41b69bde1e0eac9cdd9a966b08510207250071df
Samarth-Prajapati <samarthp475@gmail.com>
2026-03-14 20:09:56 +0530

    Add : Prompt required for content summarizer

---
+++ UrlSummarizer/prompts/summary_prompt.py
@@ -0,0 +1,39 @@
+from langchain_core.prompts import PromptTemplate
+
+class SummaryPrompt:
+    """Prompt class for summarizing content"""
+
+    def __init__(self):
+        self.template = None
+        self.prompt_template = None
+
+    def system_prompt(self):
+        """
+        Prompt Template that generate summary content according to our requirements
+        :return: prompt_template
+        """
+
+        try:
+            self.template = """
+                You are an expert content summarizer.
+
+                Summarize the following content in clear bullet points.
+                Focus on key ideas, insights, and conclusions.
+
+                Language: English
+                Limit: 300 words maximum.
+
+                CONTENT: {text}
+            """
+
+            self.prompt_template = PromptTemplate(
+                template = self.template,
+                input_variables = ["text"]
+            )
+
+            return self.prompt_template
+
+        except Exception as error:
+            print(error)
+
+prompt = SummaryPrompt().system_prompt()
